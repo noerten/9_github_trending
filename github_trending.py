636f75782d6e49b561d4cee2f27a410bc0e88ab1
noerten <darkstria@gmail.com>
2017-01-02 18:44:29 +0300

    refactored code

--- github_trending.py
+++ github_trending.py
@@ -3,12 +3,16 @@ import datetime
 import requests
 
 
-def get_trending_repositories(quantity=20):
-    date_7_days_ago = datetime.date.today() - datetime.timedelta(days=7)
+NUMBER_OF_DAYS_TO_FETCH = 7
+
+
+def get_trending_repositories(repositories_quantity=20):
+    date_n_days_ago = datetime.date.today() - datetime.timedelta(
+        days=NUMBER_OF_DAYS_TO_FETCH)
     payload = {
-        'q': 'created:>{}'.format(date_7_days_ago),
+        'q': 'created:>{}'.format(date_n_days_ago),
         'sort': 'stars',
-        'per_page': quantity,
+        'per_page': repositories_quantity,
         }
     repositories_url = 'https://api.github.com/search/repositories'
     trending_repositories = requests.get(repositories_url, params=payload)
@@ -16,22 +20,19 @@ def get_trending_repositories(quantity=20):
 
 
 def get_printable_repository_info(repository):
-    title = repository['full_name']
-    stars = repository['stargazers_count']
-    open_issues = repository['open_issues_count']
-    url = repository['html_url']
     printable_info = ('{stars} star(s), project: {title}, open issues: '
                       '{open_issues}, url: {url}'.format(
-                          stars=stars,
-                          title=title,
-                          open_issues=open_issues,
-                          url=url,
+                          stars=repository['stargazers_count'],
+                          title=repository['full_name'],
+                          open_issues=repository['open_issues_count'],
+                          url=repository['html_url'],
                           ))
     return printable_info
 
 
 if __name__ == '__main__':
     trending_repositories = get_trending_repositories()
-    print('Last 7 days trending projects on GitHub:', end='\n\n')
+    print('Last {} days trending projects on GitHub:'.format(
+        NUMBER_OF_DAYS_TO_FETCH), end='\n\n')
     for repository in trending_repositories:
         print(get_printable_repository_info(repository))
